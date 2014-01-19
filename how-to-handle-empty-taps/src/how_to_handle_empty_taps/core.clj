14500f0153a67d8a4ea265e997129164232df299
Andres Corrada-Emmanuel <andres.corrada@bluecava.com>
2014-01-19 14:16:37 -0500

    Adding directory tap.

--- how-to-handle-empty-taps/src/how_to_handle_empty_taps/core.clj
+++ how-to-handle-empty-taps/src/how_to_handle_empty_taps/core.clj
@@ -9,7 +9,24 @@
 	(let [tap (lfs-textline non-existent-file)]
 		(println "An lfs-textline tap can be created from a non-existent file!")))
 
+(defn make-tap-from-directory-with-empty-files
+	"What happens when we create a directory tap with empty source files?"
+	[dir]
+	(let [tap (lfs-textline dir :source-pattern "/part-*")]
+		(do
+			(println "We can also make taps with directories that have empty source files.")
+			(println "And the class of the tap is not nil:" (class tap)))))
+
+(defn print-tap-contents
+	"What errors do we get when we try the taps?"
+	[tap]
+	(?<- (stdout) [?line] (tap ?line)))
+
 (defn -main 
 	"What happens when different error conditions arise?"
-	[non-existent-file]
-	(make-tap-from-non-existent-lfs-file non-existent-file))
+	[non-existent-file directory-with-empty-files]
+	(do
+		(make-tap-from-non-existent-lfs-file non-existent-file)
+		(make-tap-from-directory-with-empty-files directory-with-empty-files)
+		(let [bad-tap (make-tap-from-directory-with-empty-files directory-with-empty-files)]
+			(print-tap-contents bad-tap))))
